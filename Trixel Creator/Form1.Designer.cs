b2736c9f3c1672ca1c3ed40ba795060c8f5ed698
Erik Andersson <erik_andersson007@hotmail.com>
2016-02-04 03:10:52 +0100

    Initial Commit

---
+++ Trixel Creator/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Trixel_Creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fileButton = new System.Windows.Forms.Button();
+            this.previewPictureBox = new System.Windows.Forms.PictureBox();
+            this.trixelButton = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.progressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.sizeLabel = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.previewPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Images (*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG";
+            this.openFileDialog1.Title = "Pick an Image to process";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // fileButton
+            // 
+            this.fileButton.Location = new System.Drawing.Point(13, 13);
+            this.fileButton.Name = "fileButton";
+            this.fileButton.Size = new System.Drawing.Size(79, 45);
+            this.fileButton.TabIndex = 0;
+            this.fileButton.Text = "Pick Image...";
+            this.fileButton.UseVisualStyleBackColor = true;
+            this.fileButton.Click += new System.EventHandler(this.fileButton_Click);
+            // 
+            // previewPictureBox
+            // 
+            this.previewPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.previewPictureBox.Location = new System.Drawing.Point(13, 64);
+            this.previewPictureBox.Name = "previewPictureBox";
+            this.previewPictureBox.Size = new System.Drawing.Size(559, 472);
+            this.previewPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.previewPictureBox.TabIndex = 2;
+            this.previewPictureBox.TabStop = false;
+            // 
+            // trixelButton
+            // 
+            this.trixelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.trixelButton.Enabled = false;
+            this.trixelButton.Location = new System.Drawing.Point(400, 13);
+            this.trixelButton.Name = "trixelButton";
+            this.trixelButton.Size = new System.Drawing.Size(75, 45);
+            this.trixelButton.TabIndex = 3;
+            this.trixelButton.Text = "Trixel!";
+            this.trixelButton.UseVisualStyleBackColor = true;
+            this.trixelButton.Click += new System.EventHandler(this.trixelButton_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "trixel.png";
+            this.saveFileDialog1.Filter = "Images (*.PNG)|*.PNG";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveButton.Enabled = false;
+            this.saveButton.Location = new System.Drawing.Point(482, 13);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(90, 45);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Enabled = false;
+            this.trackBar1.Location = new System.Drawing.Point(98, 13);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(296, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 35;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.progressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 539);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(584, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            this.statusStrip1.Resize += new System.EventHandler(this.statusStrip1_Resize);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.AutoSize = false;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(565, 16);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            // 
+            // sizeLabel
+            // 
+            this.sizeLabel.AutoSize = true;
+            this.sizeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sizeLabel.Location = new System.Drawing.Point(98, 43);
+            this.sizeLabel.Name = "sizeLabel";
+            this.sizeLabel.Size = new System.Drawing.Size(0, 15);
+            this.sizeLabel.TabIndex = 8;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.sizeLabel);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.trixelButton);
+            this.Controls.Add(this.previewPictureBox);
+            this.Controls.Add(this.fileButton);
+            this.MinimumSize = new System.Drawing.Size(600, 600);
+            this.Name = "Form1";
+            this.Text = "Trixel Creator";
+            ((System.ComponentModel.ISupportInitialize)(this.previewPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button fileButton;
+        private System.Windows.Forms.PictureBox previewPictureBox;
+        private System.Windows.Forms.Button trixelButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar progressBar1;
+        private System.Windows.Forms.Label sizeLabel;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Trixel Creator/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Trixel_Creator
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            if (openFileDialog1.CheckFileExists)
+            {
+                this.Activate();
+                FileStream fileStream = new FileInfo(openFileDialog1.FileName).OpenRead();
+                previewPictureBox.Image = Bitmap.FromStream(fileStream);
+                fileStream.Close();
+                trixelButton.Enabled = true;
+                trackBar1.Enabled = true;
+                trackBar1.Minimum = 1;
+                trackBar1.Maximum = previewPictureBox.Image.Width > previewPictureBox.Image.Height ? previewPictureBox.Image.Width : previewPictureBox.Image.Height;
+                sizeLabel.Text = "" + trackBar1.Value;
+            }
+
+        }
+        private Color averageCol(List<Color> colours)
+        {
+            if (colours.Count > 0)
+            {
+                int red = 0;
+                int green = 0;
+                int blue = 0;
+                foreach (Color c in colours)
+                {
+                    red += c.R;
+                    green += c.G;
+                    blue += c.B;
+                }
+                return Color.FromArgb(red / colours.Count, green / colours.Count, blue / colours.Count);
+            } else
+            {
+                return Color.FromArgb(0, 0, 0);
+            }
+
+        }
+        private void fileButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+        private void trixelButton_Click(object sender, EventArgs e)
+        {
+            Bitmap myBitmap = new Bitmap(previewPictureBox.Image);
+            progressBar1.Maximum = 100;
+            progressBar1.Step = 1;
+            progressBar1.Value = 0;
+            Tuple<Bitmap, int> indata = new Tuple<Bitmap, int>(myBitmap, trackBar1.Value);
+            backgroundWorker1.RunWorkerAsync(indata);
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            if (saveFileDialog1.FileName != "")
+            {
+                // Saves the Image via a FileStream created by the OpenFile method.
+                FileStream fs = (FileStream)saveFileDialog1.OpenFile();
+                previewPictureBox.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
+                fs.Close();
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            sizeLabel.Text = ""+trackBar1.Value;
+        }
+
+        private void statusStrip1_Resize(object sender, EventArgs e)
+        {
+            progressBar1.Width = statusStrip1.Width - 20;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            Tuple<Bitmap, int> indata = (Tuple < Bitmap, int>)e.Argument;
+            Bitmap myBitmap = indata.Item1;
+            List<Color> colours1 = new List<Color>();
+            List<Color> colours2 = new List<Color>();
+            int maximum = (myBitmap.Width * myBitmap.Height) * 2;
+            int done = 0;
+            int progress = 0;
+            int size = indata.Item2;
+            for (int i = 0; i < myBitmap.Width; i += size)
+            {
+                for (int j = 0; j < myBitmap.Height; j += size)
+                {
+                    for (int Xcount = 0; Xcount < size && i + Xcount < myBitmap.Width; Xcount++)
+                    {
+                        for (int Ycount = 0; Ycount < size && j + Ycount < myBitmap.Height; Ycount++)
+                        {
+                            done++;
+                            if ((int)Math.Round((double)(100 * done) / maximum) > progress)
+                            {
+                                progress = (int)Math.Round((double)(100 * done) / maximum);
+                                worker.ReportProgress(progress,new Bitmap(myBitmap));
+                            }
+                            if (Ycount < size - Xcount)
+                            {
+                                colours1.Add(myBitmap.GetPixel(i + Xcount, j + Ycount));
+                            }
+                            else
+                            {
+                                colours2.Add(myBitmap.GetPixel(i + Xcount, j + Ycount));
+                            }
+                        }
+                    }
+
+                    Color avg1 = averageCol(colours1);
+                    Color avg2 = averageCol(colours2);
+                    colours1 = new List<Color>();
+                    colours2 = new List<Color>();
+                    for (int Xcount = 0; Xcount < size && i + Xcount < myBitmap.Width; Xcount++)
+                    {
+                        for (int Ycount = 0; Ycount < size && j + Ycount < myBitmap.Height; Ycount++)
+                        {
+                            done++;
+                            if ((int)Math.Round((double)(100 * done) / maximum) > progress)
+                            {
+                                progress = (int)Math.Round((double)(100 * done) / maximum);
+                                worker.ReportProgress(progress, new Bitmap(myBitmap));
+                            }
+                            if (Ycount < size - Xcount)
+                            {
+                                myBitmap.SetPixel(i + Xcount, j + Ycount, avg1);
+                            }
+                            else
+                            {
+                                myBitmap.SetPixel(i + Xcount, j + Ycount, avg2);
+                            }
+                        }
+                    }
+
+                }
+            }
+            e.Result = myBitmap;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            previewPictureBox.Image = (Bitmap)e.Result;
+            saveButton.Enabled = true;
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            previewPictureBox.Image = (Bitmap)e.UserState;
+            progressBar1.Value = e.ProgressPercentage;
+        }
+    }
+}
